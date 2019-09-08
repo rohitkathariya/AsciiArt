b8a13ae8c45b9eb8c4101c75010b79f5628e3fd9
rohitkathariya <47065502+rohitkathariya@users.noreply.github.com>
2019-09-07 20:14:27 -0600

    Add files via upload

---
+++ AsciiArt.java
@@ -0,0 +1,30 @@
+
+
+/**
+ * @version date (in_ISO_8601 format: 2019-08-29) * @author rohit kathariya tharu
+ */
+public class AsciiArt {
+    /**
+     * Prints my name in ASCII Art to the console.
+     * @param args Command-line arguments are ignored. */
+    public static void main(String[] args) {
+
+        System.out.println("  _ _ _ _               _                       _ ");
+        System.out.println(" |  __ _ \\             | |           *         | | ");
+        System.out.println(" | |    ||             | |           _    _ _ _| |_ _ _    ");
+        System.out.println(" | |__ //      __      | |_ _ _ __  | |  |_ _ _| |_ _ _|       ");
+        System.out.println(" | |-\\\\      / __ \\    | |_ _ _   | | |        | |         ");
+        System.out.println(" | |   \\\\   | |__| |   | |      | | | |        | |         ");
+        System.out.println(" |_|    \\ \\  \\ __ /    |_|      |_| |_|        |_|         ");
+
+
+        }
+    }
+
+
+// Your code goes here.
+// Use as many lines as you need.
+// No line may be more than 80 character
+
+
+
